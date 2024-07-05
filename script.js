199907a8f2671cb0badbf88c99488482c0153caa
Ashim kc <ashimkc7297@gmail.com>
2024-07-05 16:21:48 +0545

    setup plus home page

---
+++ script.js
@@ -0,0 +1,43 @@
+const mainContainer = document.querySelector('.container');
+
+
+// to load home section
+const HomeButton = document.querySelector('.home-button');
+
+portfolioButton.addEventListener('click', ()=>{
+    loadPortfolio();
+})
+
+function loadPortfolio() {
+
+    mainContainer.innerHTML = '';
+
+}
+
+
+// to load portfolio section
+const portfolioButton = document.querySelector('.portfolio-button');
+
+portfolioButton.addEventListener('click', ()=>{
+    loadPortfolio();
+})
+
+function loadPortfolio() {
+
+    mainContainer.innerHTML = '';
+
+}
+
+
+// to load blog section
+const blogButton = document.querySelector('.blog-button');
+
+portfolioButton.addEventListener('click', ()=>{
+    loadPortfolio();
+})
+
+function loadPortfolio() {
+
+    mainContainer.innerHTML = '';
+
+}
